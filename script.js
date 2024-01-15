f9b1db76600ccf2f4e38ac920d29e8a9472ff0c2
Mariel Miscavage <memiscavage@gmail.com>
2024-01-15 14:45:14 -0600

    added an if statement and a variable to declare password length

--- script.js
+++ script.js
@@ -10,15 +10,23 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
 
 }
 
 // Characters required for password criteria listed here
 
 var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"; // It doesn't like when I include a backslash so that character has been left out
-var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
-var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numberCharacters = "0123456789"
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberCharacters = "0123456789";
+var passwordLength = (x < 128 || x > 8);
+
+// This is where I will prompt the page to ask about characters in the desired password
+prompt ("Enter your desired password length (8-128 characters, please):");
+  if (prompt !== passwordLength) {
+    console.log ("Please re-enter a number between 8 and 128");
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
