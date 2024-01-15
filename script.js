0ec13663cf216e7401edd084510e2c7be5890359
Mariel Miscavage <memiscavage@gmail.com>
2024-01-15 17:55:02 -0600

    declared while loop for character length and created an if statement for including special characters

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 // Assignment code here
 
+// Characters required for password criteria listed here
+
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"; // It doesn't like when I include a backslash so that character has been left out
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberCharacters = "0123456789";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Generate password function info
+
+function generatePassword() {
+  var characterPool = "";
+  var passwordLength = prompt ("Enter your desired password length (8-128 characters, please):");
+  while (passwordLength > 128 || passwordLength < 8) {
+    passwordLength = prompt ("Please re-enter a number between 8 and 128");
+  }
+  var specialCharactersConfirm = confirm ("Would you like special characters in your password?");
+  if (specialCharactersConfirm === true){
+    characterPool += specialCharacters;
+  }
+  console.log("Character pool", characterPool);
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -14,19 +35,8 @@ function writePassword() {
 
 }
 
-// Characters required for password criteria listed here
-
-var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"; // It doesn't like when I include a backslash so that character has been left out
-var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberCharacters = "0123456789";
-var passwordLength = (x < 128 || x > 8);
 
 // This is where I will prompt the page to ask about characters in the desired password
-prompt ("Enter your desired password length (8-128 characters, please):");
-  if (prompt !== passwordLength) {
-    console.log ("Please re-enter a number between 8 and 128");
-  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
